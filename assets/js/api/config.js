d43ad508bf520957bc94d040223eee4821cfe612
Aadi Bhat <114326116+aadibhat09@users.noreply.github.com>
2025-02-11 08:51:04 -0800

    Update config.js

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8101"; //Changed port here to 8101 instead of 8887
 } else {
-        pythonURI =  "https://flocker.nighthawkcodingsociety.com";
+        pythonURI =  "https://intertravel.stu.nighthawkcodingsociety.com/";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
